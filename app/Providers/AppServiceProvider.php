3ca989c0121af4fea291a8ee78cdf4edd3523718
Leonardo Augustus <leoaugusto45@gmail.com>
2025-03-08 18:59:50 -0300

    correcao nas datas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        $locale = App::currentLocale();
+
+
+
+        if (App::isLocale('en')) {
+            // ...
+        }
     }
 }
